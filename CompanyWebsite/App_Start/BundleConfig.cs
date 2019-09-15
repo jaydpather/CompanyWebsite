bf6204cbc6954222817d8e51c51bde96189931d6
Jayd <jaydpather@gmail.com>
2019-09-15 13:42:35 +0200

    home page, games, about us

--- CompanyWebsite/App_Start/BundleConfig.cs
+++ CompanyWebsite/App_Start/BundleConfig.cs
@@ -24,8 +24,10 @@ namespace CompanyWebsite
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/Content/customCss").Include(
+                      "~/Content/Site.css"));
         }
     }
 }
